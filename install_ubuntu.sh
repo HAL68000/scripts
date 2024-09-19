3523cb980c17b397412e5acb0f08b671240e26a3
Snow Oberon <ob3ron@protonmail.com>
2024-09-19 16:49:04 +0900

    Create install_ubuntu.sh

---
+++ install_ubuntu.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+apt-get update
+apt-get --yes --allow -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-new" upgrade
+apt-get install proot -y
+apt-get install proot-distro -y
+proot-distro install ubuntu
