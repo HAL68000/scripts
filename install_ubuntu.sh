a60a9103db10f3df1be3220203b966c99c14d6c1
Snow Oberon <ob3ron@protonmail.com>
2024-09-21 12:47:53 +0900

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -4,3 +4,4 @@ apt-get --yes --allow -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="-
 apt-get install proot -y
 apt-get install proot-distro -y
 proot-distro install ubuntu
+proot-distro login ubuntu -- bash -c "apt-get update && apt-get --yes --allow -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-new' upgrade"
