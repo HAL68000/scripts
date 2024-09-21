86134f02bc4d5ef42a5ab96e2312283de7d28daa
Giorgio <ob3ron@protonmail.com>
2024-09-21 14:22:34 +0900

    [refactor] dir structure3

--- install_script/install_ubuntu.sh
+++ install_script/install_ubuntu.sh
@@ -4,4 +4,4 @@ apt-get --yes --allow -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="-
 apt-get install proot -y
 apt-get install proot-distro -y
 proot-distro install ubuntu
-proot-distro login ubuntu -- bash -c "apt-get update && apt-get --yes --allow -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-new' upgrade"
+proot-distro login ubuntu -- bash -c "apt-get update && apt-get --yes -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-new' upgrade"
--- install_script/node-red.sh
+++ install_script/node-red.sh
@@ -6,7 +6,7 @@ UBUNTU_FOLDER="/data/data/com.termux/files/usr/var/lib/proot-distro/installed-ro
 # Controlla se la cartella esiste
 if [ -d "$UBUNTU_FOLDER" ]; then
     echo "Ubuntu installation found!"
-    proot-distro login ubuntu -- bash -c "apt update && apt upgrade -y && npm install -g --unsafe-perm node-red"
+    proot-distro login ubuntu -- bash -c "apt-get update && apt-get upgrade -y && apt-get install nodejs npm -y && npm install -g --unsafe-perm node-red"
 else
     echo "Ubuntu folder not found, install ubuntu first!"
 fi
