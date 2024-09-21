69829c5f76d2634d78d7d639fa8762a3e7b2ccd0
Giorgio <ob3ron@protonmail.com>
2024-09-21 13:09:31 +0900

    [refactor] dir structure

--- install_script/node-red.sh
+++ install_script/node-red.sh
@@ -1,17 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Percorso della cartella da controllare
-UBUNTU_FOLDER="/percorso/della/cartella"
+UBUNTU_FOLDER="/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu"
 
 # Controlla se la cartella esiste
 if [ -d "$UBUNTU_FOLDER" ]; then
     echo "Ubuntu installation found!"
-    
-    apt update
-    apt upgrade -y
-    npm install -g --unsafe-perm node-red
-    
-
+    proot-distro login ubuntu -- bash -c "apt update && apt upgrade -y && npm install -g --unsafe-perm node-red"
 else
     echo "Ubuntu folder not found, install ubuntu first!"
 fi
